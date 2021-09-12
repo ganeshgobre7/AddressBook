56c8d3189db2e8600d93c51282694d476b7c1a08
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-12 12:42:23 +0530

    Welcome message added

---
+++ AddressBookMain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookMain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wel-Come to the Address Book Program");
+        }
+    }
+}
